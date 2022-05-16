a2494c57e296f4590d9563ef689b748abb21da97
Mieker <mieker@wp.pl>
2022-05-16 22:38:27 +0200

    dice css styled and animated

--- src/components/UI/Dice.js
+++ src/components/UI/Dice.js
@@ -1,11 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import classes from "./Dice.module.css";
 
 const Dice = (props) => {
+  const [classesSet, setClassesSet] = useState(classes.dice);
+
+  const clickHandler = () => {
+    setClassesSet(`${classes.dice} ${classes.dice_rolling}`);
+    const myTimeout = setTimeout(function () {
+      setClassesSet(classes.dice);
+    }, 500);
+
+    props.onClick();
+  };
+
   return (
     <Fragment>
-      <button className={classes.dice} onClick={props.onClick}>
+      <button className={classesSet} onClick={clickHandler}>
         {props.children}
       </button>
     </Fragment>
