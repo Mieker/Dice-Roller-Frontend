e169745e3ca6e3972970b12c27c515929c970b07
Mieker <mieker@wp.pl>
2022-05-17 21:49:29 +0200

    all dnd dices styled and rolled

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,31 +1,22 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import Dice from "../UI/Dice";
 
+import d20img from "../UI/DiceSVG/d20.svg";
+import d12img from "../UI/DiceSVG/d12.svg";
+import d10img from "../UI/DiceSVG/d10.svg";
+import d8img from "../UI/DiceSVG/d8.svg";
+import d6img from "../UI/DiceSVG/d6.svg";
+import d4img from "../UI/DiceSVG/d4.svg";
+
 const Board = () => {
-  const [throwResult, setThrowResult] = useState(6);
-
-  const throwHandler = (props) => {
-    let url = "https://dicethrowerapp.herokuapp.com/roll/d6";
-    // let url = "http://localhost:8080/roll/d6";
-
-    fetch(url, {
-      mode: "cors",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setThrowResult(data.throw);
-      });
-  };
-
   return (
     <Fragment>
-      <Dice onClick={throwHandler}>{throwResult}</Dice>
+      <Dice diceValue={20} diceImg={d20img} />
+      <Dice diceValue={12} diceImg={d12img} />
+      <Dice diceValue={10} diceImg={d10img} />
+      <Dice diceValue={8} diceImg={d8img} />
+      <Dice diceValue={6} diceImg={d6img} />
+      <Dice diceValue={4} diceImg={d4img} />
     </Fragment>
   );
 };
--- src/components/UI/Dice.js
+++ src/components/UI/Dice.js
@@ -4,6 +4,8 @@ import classes from "./Dice.module.css";
 
 const Dice = (props) => {
   const [classesSet, setClassesSet] = useState(classes.dice);
+  const [throwResult, setThrowResult] = useState(props.diceValue);
+  
 
   const clickHandler = () => {
     setClassesSet(`${classes.dice} ${classes.dice_rolling}`);
@@ -11,14 +13,37 @@ const Dice = (props) => {
       setClassesSet(classes.dice);
     }, 500);
 
-    props.onClick();
+    let url = "https://dicethrowerapp.herokuapp.com/roll/d" + props.diceValue;
+    // let url = "http://localhost:8080/roll/d" + props.diceValue;
+
+    console.log(url)
+    fetch(url, {
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setThrowResult(data.throw);
+      });
   };
 
   return (
     <Fragment>
-      <button className={classesSet} onClick={clickHandler}>
-        {props.children}
-      </button>
+      <div
+        className={classesSet}
+        onClick={clickHandler}
+        role="button"
+        tabIndex="0"
+        diceValue={props.diceValue}
+      >
+        <img src={props.diceImg} alt="dnd dice" />
+        <p>{throwResult}</p>
+      </div>
     </Fragment>
   );
 };
